fefd1b29bde6b4417b423df5c8eb87b83522e342
SGrimsley02 <99296361+SGrimsley02@users.noreply.github.com>
2023-04-15 09:57:08 -0500

    Added for loop to locations

--- lab.py
+++ lab.py
@@ -1,25 +1,28 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-location = "Kans."
-# keep KEYLEN and OFFSET const
-KEYLEN = 500
-OFFSET = 0
-
+locations = ["Kans."]
 data = {}
 
-while KEYLEN == 500:
-    url = f"http://floranorthamerica.org/Special:Ask/format%3Djson/sort%3D/order%3Dasc/offset%3D{OFFSET}/limit%3D500/-5B-5BDistribution::{location}-5D-5D/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue/searchlabel%3DJSON"
+for location in locations:
+    # keep KEYLEN and OFFSET const
+    KEYLEN = 500
+    OFFSET = 0
 
-    res = requests.get(url).json()
-    keys = res["results"].keys()
-    KEYLEN = len(keys)
+    
 
-    OFFSET += 500
+    while KEYLEN == 500:
+        url = f"http://floranorthamerica.org/Special:Ask/format%3Djson/sort%3D/order%3Dasc/offset%3D{OFFSET}/limit%3D500/-5B-5BDistribution::{location}-5D-5D/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue/searchlabel%3DJSON"
 
-    print("Key Length: ", KEYLEN)
+        res = requests.get(url).json()
+        keys = res["results"].keys()
+        KEYLEN = len(keys)
 
-    data.update(res["results"])
+        OFFSET += 500
+
+        print("Key Length: ", KEYLEN)
+
+        data.update(res["results"])
 
 # http://floranorthamerica.org/Thalictrum_thalictroides
 flower_url = "http:" + data["Thalictrum thalictroides"]["fullurl"]
