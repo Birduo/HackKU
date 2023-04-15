4303d536e5750d89b19a44381ef5426db6e2c29a
SGrimsley02 <sam.grimsley@gmail.com>
2023-04-15 10:05:32 -0500

    Update lab.py

--- lab.py
+++ lab.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 locations = ["Kans."]
-data = {}
 
+state_flowers = {}
 for location in locations:
     # keep KEYLEN and OFFSET const
     KEYLEN = 500
     OFFSET = 0
 
-    
+    data = {}
 
     while KEYLEN == 500:
         url = f"http://floranorthamerica.org/Special:Ask/format%3Djson/sort%3D/order%3Dasc/offset%3D{OFFSET}/limit%3D500/-5B-5BDistribution::{location}-5D-5D/mainlabel%3D/prettyprint%3Dtrue/unescape%3Dtrue/searchlabel%3DJSON"
@@ -23,6 +23,8 @@ for location in locations:
         print("Key Length: ", KEYLEN)
 
         data.update(res["results"])
+    
+    state_flowers[location] = list(data.keys())
 
 # http://floranorthamerica.org/Thalictrum_thalictroides
 flower_url = "http:" + data["Thalictrum thalictroides"]["fullurl"]
@@ -31,4 +33,7 @@ flower = requests.get(flower_url)
 
 soup = bs(flower.content, "html.parser")
 
-print(soup)
+
+
+
+print(state_flowers)
